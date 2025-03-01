845173fb0600d728fe0a546a191cca4d6e366158
dlaqudrl159 <qkfkaxkrl@gmail.com>
2025-03-01 16:49:32 +0900

    MainController 추가

---
+++ src/main/java/info/controller/MainController.java
@@ -0,0 +1,20 @@
+package info.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import info.domain.ControllerAction;
+
+public class MainController implements Controller{
+
+	@Override
+	public ControllerAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
